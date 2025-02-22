d553279dd52abe200b86b43a957fe90f38aebd18
Biswajitmahal <89303516+Biswajitmahal@users.noreply.github.com>
2025-02-22 20:43:59 +0000

    comparison and more on data types

---
+++ Basic_of_JS/comaprison.js
@@ -0,0 +1,15 @@
+console.log("2">1); //true coverts the string to Number
+
+
+console.log(null>0);//false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+/* 
+Note : The reason is that an equality check == and comparisons > < >= <= work defferentlu
+comparision converts null to 0 
+so null>=0 is true and null>0 false
+
+*/
+
+
--- Basic_of_JS/datatypes.js
+++ Basic_of_JS/datatypes.js
@@ -4,15 +4,22 @@ let name ="rock" //string
 let age= 22 //number
 let islog=true //booleans
 
-/* Data types are
+/* Data types are 
+ Primitives
    1.number => 2 to power53
    2.string => ""
    3.boolean => true || false
    4.null => standalone value
    5.undefined =>when you dont assign value 
    6.symbol =>uniqueness 
-   7.Object
+   7.BigInt
+
+ Reference(Non Primitives)
+   1.Array
+   2.Objects
+   3.Functions
 */
+
 console.log(typeof undefined); //undefined
 console.log(typeof null); //object
 
